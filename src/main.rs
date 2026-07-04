359789dea1fff6c39fb9bb9184f247029e6cf06f
Rixzik <ueuerejej@gmail.com>
2026-07-04 14:34:49 +0400

    Refactor structs and methods for lifetime parameters

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use core::panic;
+
 mod token;
 use token::Token;
 use token::tokenize;
 
 #[derive(Debug, Clone)]
-struct Param {
-    name: String,
+struct Param<'a>{
+    name: &'a str,
     ty: Type,
 }
 
@@ -22,57 +23,57 @@ enum BinaryOp {
     Div,
 }
 #[derive(Debug, Clone)]
-pub struct Func {
-    args: Vec<Param>,
-    code: Vec<Stmt>,
-    name: String,
+pub struct Func<'a> {
+    args: Vec<Param<'a>>,
+    code: Vec<Stmt<'a>>,
+    name: &'a str,
 }
 #[derive(Debug, Clone)]
-pub struct Func_call {
-    name: String,
-    args: Vec<Expr>,
+pub struct Func_call<'a> {
+    name: &'a str,
+    args: Vec<Expr<'a>>,
 }
 #[derive(Debug, PartialEq, Clone)]
-pub enum Expr {
+pub enum Expr<'a>{
     Num(i64),
-    Id(String),
+    Id(&'a str),
     Float(f64),
-    Str(String),
+    Str(&'a str),
     Comma,
 
     Binary {
-        left: Box<Expr>,
+        left: Box<Expr<'a>>,
         op: BinaryOp,
-        right: Box<Expr>,
+        right: Box<Expr<'a>>,
     },
 }
-#[derive(Debug, Clone)]
+#[derive(Debug,Clone)]
 
-pub struct Var {
+pub struct Var<'a>{
     tipe: Type,
-    value: Expr,
-    name: String,
+    value: Expr<'a>,
+    name: &'a str,
 }
 #[derive(Debug, Clone)]
-pub enum Stmt {
-    Int(Var),
-    Float(String, Expr),
-    Str(String, Expr),
-    Var(String),
-    ReturnStmt(Expr),
-    Func(Func),
-    Func_call(Func_call),
-    Binary(Expr),
-    Expr(Expr),
+pub enum Stmt<'a>{
+    Int(Var<'a>),
+    Float(& 'a str, Expr<'a>),
+    Str(&'a str,Expr<'a>),
+    Var(&'a str),
+    ReturnStmt(Expr<'a>),
+    Func(Func<'a>),
+    Func_call(Func_call<'a>),
+    Binary(Expr<'a>),
+    Expr(Expr<'a>),
 }
-struct Parser {
-    tokens: Vec<Token>,
+struct Parser<'a>{
+    tokens: Vec<Token<'a>>,
     pos: usize,
-    func_names: Vec<Func>,
+    func_names: Vec<Func<'a>>,
 }
 
-impl Parser {
-    fn new(tokens: Vec<Token>) -> Self {
+impl <'a>Parser <'a>{
+    fn new(tokens: Vec<Token<'a>>) -> Self {
         Parser {
             tokens,
             pos: 0,
@@ -80,36 +81,35 @@ impl Parser {
         }
     }
 
-    fn current(&self) -> &Token {
-        &self.tokens[self.pos]
+    fn current(&self) -> Token<'a> {
+        self.tokens[self.pos]
     }
 
-    fn advance(&mut self) -> &Token {
-        if self.pos < self.tokens.len() - 1 {
-            self.pos += 1;
-        }
-        &self.tokens[self.pos - 1]
-    }
+   fn advance(&mut self) -> Token<'a> {
+    let tok = self.tokens[self.pos].clone();
+    self.pos += 1;
+    tok
+}
 
-    fn previous(&self) -> &Token {
-        &self.tokens[self.pos - 1]
+    fn previous(&self) -> Token<'a> {
+        self.tokens[self.pos - 1]
     }
-    fn parse(&mut self) -> Vec<Stmt> {
+    fn parse(&  mut self) -> Vec<Stmt<'a>> {
         let mut statements = Vec::new();
-        while *self.current() != Token::EOF {
+        while self.current() != Token::EOF {
             statements.push(self.parse_statement());
         }
 
         statements
     }
 
-    fn parse_return(&mut self) -> Stmt {
+    fn parse_return(&mut self) -> Stmt<'a> {
         self.advance();
         let expr = self.parse_primary();
 
-        Stmt::ReturnStmt(expr)
+        Stmt::ReturnStmt( expr)
     }
-    fn parse_int(&mut self) -> Stmt {
+    fn parse_int(& mut self) -> Stmt<'a> {
         self.advance();
 
         let name = match self.advance().clone() {
@@ -117,19 +117,19 @@ impl Parser {
             _ => panic!("Expected identifier"),
         };
 
-        if *self.advance() != Token::Assign {
+        if self.advance() != Token::Assign {
             panic!("Expected '='");
         }
 
         let expr = self.chech_expr(Type::Int);
 
-        Stmt::Int(Var {
+        Stmt::Int(Var  {
             tipe: Type::Int,
             value: expr,
             name,
         })
     }
-    fn chech_expr(&mut self, tipe: Type) -> Expr {
+    fn chech_expr(&mut self, tipe: Type) -> Expr<'a> {
         let expr = self.parse_expr();
         match (tipe.clone(), expr.clone()) {
             (Type::Int, Expr::Num(_))
@@ -143,7 +143,7 @@ impl Parser {
         }
      
     }
-    fn parse_func_call(&mut self) -> Stmt {
+    fn parse_func_call(&mut self) -> Stmt<'a> {
         let mut args: Vec<Expr> = Vec::new();
 
         let name = match self.advance().clone() {
@@ -152,8 +152,8 @@ impl Parser {
         };
 
         self.advance();
-        while *self.current() != Token::Rparen {
-            if *self.current() == Token::Comma {
+        while self.current() != Token::Rparen {
+            if self.current() == Token::Comma {
                 self.advance();
             }
             args.push(self.parse_expr());
@@ -161,40 +161,40 @@ impl Parser {
         self.advance();
         Stmt::Func_call(Func_call { name, args })
     }
-    fn parse_func(&mut self) -> Stmt {
+    fn parse_func(&mut self) -> Stmt<'a> {
         self.advance();
 
         let mut args: Vec<Param> = Vec::new();
-        let mut code_token: Vec<Token> = Vec::new();
-        let name = match self.advance().clone() {
+        let mut code_token: Vec<Token<'a>> = Vec::new();
+        let name = match self.advance() {
             Token::Identifier(name) => name,
             _ => panic!("Expected identifier"),
         };
 
-        if *self.advance() != Token::Lparen {
+        if self.advance() != Token::Lparen {
             panic!("Expected '('")
         }
 
-        while *self.current() != Token::Rparen {
+        while self.current() != Token::Rparen {
             if let typee = self.parse_func_args() {
-                let name = match self.advance().clone() {
+                let name = match self.advance() {
                     Token::Identifier(name) => name,
                     _ => panic!("Expected identifier"),
                 };
                 args.push(Param { name, ty: typee });
             };
 
-            if *self.current() == Token::Comma {
+            if self.current() == Token::Comma {
                 self.advance();
             }
         }
 
         self.advance();
-        if *self.advance() != Token::Lcurly {
+        if self.advance() != Token::Lcurly {
             panic!("Expected Lcurly ")
         }
-        while *self.current() != Token::Rcurly {
-            code_token.push(self.current().clone());
+        while self.current() != Token::Rcurly {
+            code_token.push(self.current());
             self.advance();
         }
 
@@ -203,11 +203,11 @@ impl Parser {
 
         let mut parser = Parser::new(code_token.clone());
 
-        println!("{:?}", code_token.clone());
+        println!("{:?}", code_token);
         Stmt::Func(Func {
             args,
             code: parser.parse(),
-            name,
+            name: name,
         })
     }
 
@@ -219,7 +219,7 @@ impl Parser {
             _ => panic!("Expected type "),
         }
     }
-    fn parse_str(&mut self) -> Stmt {
+    fn parse_str(&mut self) -> Stmt<'a> {
         self.advance();
 
         let name = match self.advance().clone() {
@@ -227,7 +227,7 @@ impl Parser {
             _ => panic!("Expected identifier"),
         };
 
-        if *self.advance() != Token::Assign {
+        if self.advance() != Token::Assign {
             panic!("Expected '='");
         }
 
@@ -239,17 +239,17 @@ impl Parser {
             panic!("Expected string");
         }
     }
-    fn next(&mut self) -> &Token {
-        &self.tokens[self.pos + 1]
+    fn next(&mut self) -> Token<'a> {
+        self.tokens[self.pos + 1]
     }
-    fn parse_statement(&mut self) -> Stmt {
-        match self.current().clone() {
+    fn parse_statement(&  mut self) -> Stmt<'a>{
+        match self.current() {
             Token::Int => self.parse_int(),
             Token::Str => self.parse_str(),
             Token::Return => self.parse_return(),
             Token::Func => self.parse_func(),
             Token::Identifier(name) => {
-                if *self.next() == Token::Lparen {
+                if self.next() == Token::Lparen {
                     self.parse_func_call()
                 } else {
                     panic!("sas")
@@ -263,8 +263,8 @@ impl Parser {
         }
     }
 
-    fn parse_primary(&mut self) -> Expr {
-        let token = self.advance().clone();
+    fn parse_primary(&mut self) -> Expr<'a> {
+        let token = self.advance();
         match token {
             Token::Number(val) => Expr::Num(val),
             Token::Identifier(name) => Expr::Id(name),
@@ -273,15 +273,15 @@ impl Parser {
         }
     }
 
-        fn parse_primary_for_bianry(&mut self) -> Expr {
-        let token = self.advance().clone();
+        fn parse_primary_for_bianry(&mut self) -> Expr<'a> {
+        let token = self.advance();
         match token {
             Token::Number(val) => Expr::Num(val),
             Token::Identifier(name) => Expr::Id(name),
        
                         Token::Lparen=>{
                           let expr =  self.parse_expr();
-                            if *self.current() != Token::Rparen{
+                            if self.current() != Token::Rparen{
                                 panic!("Expected ')'")
                             }
                             self.advance();
@@ -292,7 +292,7 @@ impl Parser {
             _ => panic!("error{:?}", token),
         }
     }
-    fn parse_expr(&mut self) -> Expr {
+    fn parse_expr(&mut self) -> Expr<'a> {
         let mut left = self.parse_term();
         while matches!(self.current(), Token::Mines | Token::Add) {
             let op = match self.advance() {
@@ -311,7 +311,7 @@ impl Parser {
         }
         left
     }
-    fn parse_term(&mut self) -> Expr {
+    fn parse_term(&mut self) -> Expr<'a>{
         let mut left = self.parse_primary_for_bianry();
 
         while matches!(self.current(), Token::Mul | Token::Div) {
@@ -337,7 +337,7 @@ impl Parser {
         }
     }
 }
-pub fn ready_code(code: &str) -> Vec<Stmt> {
+pub fn ready_code<'a>(code: &'a str) -> Vec<Stmt<'a>> {
     let tokens = tokenize(code);
 
     let mut parser = Parser::new(tokens);
@@ -355,4 +355,5 @@ fn main() {
     println!("{:?}", emc);
     println!("{:?}", parser.parse());
 }
+
 
