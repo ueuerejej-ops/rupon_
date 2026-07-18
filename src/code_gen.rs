3493966f39432c7077620b7b978e80cd91ea6267
Rixzik <ueuerejej@gmail.com>
2026-07-18 18:21:56 +0400

    Update and rename main.rs to code_gen.rs

--- src/code_gen.rs
+++ src/code_gen.rs
@@ -2,19 +2,33 @@ use fxhash::FxHashMap;
 
 use inkwell::values::BasicValue;
 use inkwell::values::PointerValue;
-mod parser;
-mod arena;
-mod token;
-use parser::Type;
-use parser::Var;
-struct Compiler<'ctx> {
-    context: &'ctx Context,
-    builder: Builder<'ctx>,
-    module: Module<'ctx>,
-    variables: SymbolHash<'ctx>,
-    strint: StringInterner<'ctx>,
+use inkwell::values::FunctionValue;
+
+use crate::parser::Type;
+use crate::parser::Func_call;
+use crate::parser::Param;
+
+use crate::parser::Var;
+struct FunctionTable<'ctx> {
+    funcs: FxHashMap<usize, FunctionValue<'ctx>>,
 }
 
+
+impl <'ctx>FunctionTable<'ctx>{
+    fn new()->Self{
+        Self { funcs:  FxHashMap::default()}
+    }
+
+}
+
+
+pub struct Compiler<'ctx,'src> {
+   pub context: &'ctx Context,
+  pub  builder: Builder<'ctx>,
+   pub module: Module<'ctx>,
+   pub variables: SymbolHash<'ctx>,
+   pub strint: StringInterner<'src>,
+}
 
 use crate::arena::Arena;
 use crate::parser::Expr;
@@ -28,13 +42,14 @@ use inkwell::values::{
    BasicValueEnum, 
 };
 
-#[derive(Debug, Clone)]
-struct Varaibeldata<'ctx> {
-    ty: Type,
+#[derive(Debug, Clone,PartialEq)]
+
+pub struct Varaibeldata<'ctx> {
+    pub ty: Type,
     ptr: PointerValue<'ctx>,
 }
 
-impl<'a> StringInterner<'a> {
+impl<'src> StringInterner<'src> {
     fn new() -> Self {
         StringInterner {
             string: Vec::new(),
@@ -42,7 +57,7 @@ impl<'a> StringInterner<'a> {
         }
     }
 
-    fn itern(&mut self, name: &'a str) -> usize {
+   pub fn itern(&mut self, name: &'src str) -> usize {
         if let Some(&id) = self.map.get(name) {
             return id;
         } else {
@@ -52,7 +67,8 @@ impl<'a> StringInterner<'a> {
             id
         }
     }
-    fn lookup(&self, name: &'a str) -> usize {
+    
+ pub   fn lookup(&self, name: &'src str) -> usize {
         if let Some(id) = self.map.get(name) {
             *id
         } else {
@@ -60,12 +76,16 @@ impl<'a> StringInterner<'a> {
         }
     }
 }
-struct StringInterner<'a> {
-    string: Vec<&'a str>,
-    map: FxHashMap<&'a str, usize>,
+#[derive(Debug, PartialEq)]
+
+pub struct StringInterner<'src> {
+    pub string: Vec<&'src str>,
+    map: FxHashMap<&'src str, usize>,
 }
 
-struct SymbolHash<'ctx> {
+#[derive(Debug, Clone,PartialEq)]
+
+pub struct SymbolHash<'ctx> {
     variebles: FxHashMap<usize, Varaibeldata<'ctx>>,
 }
 impl<'ctx> SymbolHash<'ctx> {
@@ -75,15 +95,15 @@ impl<'ctx> SymbolHash<'ctx> {
         }
     }
 
-    fn save(&mut self, var: Varaibeldata<'ctx>, id: usize) {
+   pub fn save(&mut self, var: Varaibeldata<'ctx>, id: usize) {
         self.variebles.insert(id, var);
     }
-    fn get_var(&self, id: usize) -> Option<&Varaibeldata<'ctx>> {
+   pub fn get_var(&self, id: usize) -> Option<&Varaibeldata<'ctx>> {
         self.variebles.get(&id)
     }
 }
-impl<'ctx> Compiler<'ctx> {
-    fn new(context: &'ctx Context, module_name: &str) -> Self {
+impl<'ctx,'src> Compiler<'ctx,'src> {
+   pub  fn new(context: &'ctx Context, module_name: &str) -> Self {
         let compiler = Compiler {
             context,
             builder: context.create_builder(),
@@ -111,7 +131,7 @@ fn get_expr_type(&self, expr: &Expr) -> Type {
         _ => panic!("unknown")
     }
 }
-    fn create_var<'a>(&mut self, name: &'ctx str, value: BasicValueEnum<'ctx>, ty: Type) {
+    fn create_var(&mut self, name: &'src str, value: BasicValueEnum<'ctx>, ty: Type) {
               let ptr =   match ty{
                   Type::Int=>{
                     self.builder.build_alloca(self.context.i64_type(), name).unwrap()
@@ -130,6 +150,23 @@ fn get_expr_type(&self, expr: &Expr) -> Type {
          self.variables.save(Varaibeldata { ty, ptr }, id);
         
     }
+   pub  fn get_type_our_expr<'a>(&mut self,expr: Expr<'a>)->Type{
+    match expr{
+        Expr::Num(_)=>Type::Int,
+        Expr::Str(_)=>Type::Str,
+        Expr::Id(name)=>{
+           let id = self.strint.lookup(name);
+        let var = self.variables.get_var(id).unwrap().clone();
+         var.ty
+        } 
+        _=>panic!("e")
+    }
+}
+    fn func_create(&mut self,parametrs: Param,name: &str){
+
+    }
+
+
 
     fn get_value_of_expr<'a>(&mut self, expr: Expr<'a>) -> Option<BasicValueEnum<'ctx>> {
         match expr {
@@ -172,7 +209,7 @@ Some(value)
             _ => panic!("error expr"),
         }
     }
-    fn  read_stmt<'a>(&mut self,var: Var<'a>) where 'a: 'ctx{
+    fn  read_stmt(&mut self,var: Var<'src>){
         unsafe{
           let  expr =  &*var.value;
         let expr_type = self.get_expr_type(expr);
@@ -197,15 +234,45 @@ Some(value)
         }
 
 }
-fn parse_stmt<'a>(&mut self,stmts: Vec<Stmt<'a>>) where 'a : 'ctx{
+fn do_return(&mut self,expr: *mut Expr){
+unsafe {
+    let expr = &mut *expr;
+    let basic_value = self.get_value_of_expr(expr.clone()).unwrap();
+    let _ = self.builder.build_return(Some(&basic_value));
+}
+}
+fn assign_var(&mut self,name: &'src str,expr: *mut Expr<'src>){
+   unsafe {
+    if self.strint.string.contains(&name){
+        let id = self.strint.lookup(name);
+    let mut expr = &mut *expr;
+        let expr_type = self.get_expr_type(expr);
+
+    let basic_value = self.get_value_of_expr(expr.clone()).unwrap();
+   let var = self.variables.get_var(id).unwrap();
+   if var.ty != expr_type{
+    panic!("type error")
+   }
+           self.builder.build_store(var.ptr, basic_value).unwrap();
+
+    }
+    
+
+   }
+}
+pub fn parse_stmt(&mut self,stmts: Vec<Stmt<'src>>) {
   for stmt in stmts{
     match stmt{
-      Stmt::Int(var)| Stmt::Str(var)=>{
+      Stmt::Int(var)| Stmt::Str(var) =>{
 self.read_stmt(var);
   
        }
-      
-       
+      Stmt::Assign { name, value }=>{
+       self.assign_var(name, value.clone());
+    }
+       Stmt::ReturnStmt(value)=>{
+        self.do_return(value);
+       }
            _ =>  panic!("")
         
     }
@@ -216,26 +283,5 @@ self.read_stmt(var);
 
 }
 
-fn main() {
-    let mut arena = Arena::new(1000);
-    let context = Context::create();
-    let mut compiler = Compiler::new(&context, "arm_module");
 
-
-
-    let fn_type = context.void_type().fn_type(&[], false);
-    let function = compiler.module.add_function("test", fn_type, None);
-
-
-    let entry = compiler.context.append_basic_block(function, "entry");
-
-    compiler.builder.position_at_end(entry);
-    let code_my = r#"str df = 3 int he = df
-
-  "#;
-let stmt = ready_code(&mut arena as *mut Arena ,code_my);
-compiler.parse_stmt(stmt.clone());
-println!("{:?}",stmt);
-    compiler.module.print_to_stderr();
-}
 
