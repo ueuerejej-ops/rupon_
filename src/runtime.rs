3a3708e389c150c1f7ca9bdb6912f5df94d9178f
Rixzik <ueuerejej@gmail.com>
2026-08-24 13:56:09 +0400

    Update runtime.rs

--- src/runtime.rs
+++ src/runtime.rs
@@ -1,3 +1,4 @@
+use libc::CS;
 use std::ffi::CStr;
 use std::io::{self, Write};
 
@@ -9,6 +10,15 @@ impl Runtime {
     pub fn new() -> Self {
         Self {
             stdout: io::stdout(),
+        }
+    }
+
+    pub fn compare_str(&mut self, ptr1: *const u8, ptr2: *const u8) -> bool {
+        unsafe {
+            let text1 = CStr::from_ptr(ptr1 as *const i8).to_bytes();
+            let text2 = CStr::from_ptr(ptr2 as *const i8).to_bytes();
+
+            text1 == text2
         }
     }
 
@@ -27,5 +37,16 @@ pub extern "C" fn runtime_print(runtime: *mut Runtime, ptr: *const u8) {
     unsafe {
         let runtime = &mut *runtime;
         runtime.print(ptr);
+    }
+}
+#[unsafe(no_mangle)]
+pub extern "C" fn runtime_compare_str(
+    runtime: *mut Runtime,
+    ptr1: *const u8,
+    ptr2: *const u8,
+) -> bool {
+    unsafe {
+        let runtime = &mut *runtime;
+        runtime.compare_str(ptr1, ptr2)
     }
 }
